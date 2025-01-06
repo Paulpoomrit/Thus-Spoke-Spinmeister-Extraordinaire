6625365f9e6c906d67c90ccbffd6e61be7eccbaf
Paulpoomrit <paulpoomrit@gmail.com>
2025-01-06 11:27:43 -0800

    BaseRetroWeapon Setup #1

--- Source/ThusSpokeSpinmeister/BaseRetroWeapon.cpp
+++ Source/ThusSpokeSpinmeister/BaseRetroWeapon.cpp
@@ -1,29 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Gun.h"
+#include "BaseRetroWeapon.h"
 
 // Sets default values
-AGun::AGun()
+ABaseRetroWeapon::ABaseRetroWeapon()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Set Up Mesh
-	UStaticMeshComponent* Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GunMesh"));
-	Mesh->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
-void AGun::BeginPlay()
+void ABaseRetroWeapon::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// Assign 
+	
 }
 
 // Called every frame
-void AGun::Tick(float DeltaTime)
+void ABaseRetroWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
