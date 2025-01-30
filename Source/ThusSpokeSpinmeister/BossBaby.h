b581b035b04a071262b036b7f9fad6040be62838
Paulpoomrit <paulpoomrit@gmail.com>
2025-01-30 08:31:01 -0800

    HealthBar Added for Boss Baby

---
+++ Source/ThusSpokeSpinmeister/BossBaby.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Baby.h"
+#include "BossBaby.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THUSSPOKESPINMEISTER_API ABossBaby : public ABaby
+{
+	GENERATED_BODY()
+	
+};
